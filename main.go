5bbeec8429e91bbf01125dc8893ae30ede6569e3
herulobarto <herulobarto@gmail.com>
2024-06-03 11:57:41 +0700

    membuat setup untuk koneksi ke database

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/herulobarto/go-restapi-mux/controllers/productcontroller"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/products", productcontroller.Index).Methods("GET")
+	r.HandleFunc("/product/{id}", productcontroller.Show).Methods("GET")
+	r.HandleFunc("/product", productcontroller.Create).Methods("POST")
+	r.HandleFunc("/product/{id}", productcontroller.Update).Methods("PUT")
+	r.HandleFunc("/product", productcontroller.Delete).Methods("DELETE")
+
+	http.ListenAndServe(":8080", r)
+
+}
