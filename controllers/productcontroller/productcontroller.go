bf4429f4986b3efb21d25cbf3699103a6f659fcb
herulobarto <herulobarto@gmail.com>
2024-06-03 12:17:12 +0700

    membuat func show untuk menampilkan product berdasarkan id

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -2,9 +2,12 @@ package productcontroller
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/herulobarto/go-restapi-mux/helper"
 	"github.com/herulobarto/go-restapi-mux/models"
+	"gorm.io/gorm"
 )
 
 var ResponseJson = helper.ResponseJson
@@ -23,6 +26,25 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func Show(w http.ResponseWriter, r *http.Request) {
 
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+	}
+
+	var product models.Product
+	if err := models.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			ResponseError(w, http.StatusNotFound, "Product tidak ditemukan")
+			return
+		default:
+			ResponseError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
